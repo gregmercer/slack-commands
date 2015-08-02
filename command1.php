8a798bd22d794909c95e4a04d89cde4b959185da
Greg Mercer (GBear) <gmercer@stanford.edu>
2015-08-01 18:57:54 -0700

    Check token passed to command

--- command1.php
+++ command1.php
@@ -2,6 +2,12 @@
 
 $command = $_POST['command'];
 $command_arg1 = $_POST['text'];
+$token = $_POST['token'];
 
-echo 'hello ' . $command_arg1 . ' from command1';
+$got_token = 'no token found.';
+if ($token == 'cr6Z62AwRRz22km0el54C82P') {
+  $got_token = 'token found';
+}
+
+echo 'hello ' . $command_arg1 . ' from command1' . ' ' . $got_token;
 
